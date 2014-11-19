f06433dbb56839b100aa5bac0b5d51e3c7cc14ae
ischneider <ischneider@localhost>
2014-11-19 17:33:40 -0600

    Restaurant business created.

---
+++ business/restaurantBS.php
@@ -0,0 +1,52 @@
+<?php
+
+	include_once "mapper/restaurantMapper.php";
+	include_once "model/model.restaurant.php";
+
+	class RestaurantBS
+	{
+		private $restaurantMapper = null;
+
+		function RestaurantBS()
+		{
+			$this->restaurantMapper = new RestaurantMapper();
+		}
+
+		public function getRestaurants()
+		{
+			return $this->restaurantMapper->getRestaurants();
+		}
+
+
+		public function addRestaurant()
+		{
+			$this->restaurantMapper->addRestaurant(getPostData());
+		}
+		
+		public function updateRestaurant()
+		{
+			$this->restaurantMapper->updateRestaurant(getPostData());
+		}
+		
+		public function deleteRestaurant()
+		{
+			$this->restaurantMapper->deleteRestaurant($_POST['id']);
+		}
+		
+		private function getPostData()
+		{
+			$restaurant = new restaurant();
+			
+			$restaurant->id = $_POST['id'];
+			$restaurant->name = $_POST['name'];
+			$restaurant->phone = $_POST['phone'];
+			$restaurant->addressLine1 = $_POST['addressLine1'];
+			$restaurant->addressLine2 = $_POST['addressLine2'];
+			$restaurant->city = $_POST['city'];
+			$restaurant->state = $_POST['state'];
+			$restaurant->country = $_POST['country'];
+			$restaurant->postalCode = $_POST['postalCode'];
+			
+			return $restaurant;
+		}
+	}
