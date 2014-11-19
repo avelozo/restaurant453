60df06f0ec35d52af368bea87bc277bad3b53b37
Luiz Miranda <lmiranda2@luc.edu>
2014-11-19 13:22:44 -0600

    DDL changed to concatenate the table name in the columns.

--- Documents/ddl.sql
+++ Documents/ddl.sql
@@ -16,13 +16,13 @@ DROP TABLE IF EXISTS `customer` ;
 CREATE TABLE IF NOT EXISTS `customer` (
   `customerId` INT(11) NOT NULL AUTO_INCREMENT,
   `customerName` VARCHAR(50) NOT NULL,
-  `phone` VARCHAR(50) NOT NULL,
-  `addressLine1` VARCHAR(50) NOT NULL,
-  `addressLine2` VARCHAR(50) NULL DEFAULT NULL,
-  `city` VARCHAR(50) NOT NULL,
-  `state` VARCHAR(50) NULL DEFAULT NULL,
-  `country` VARCHAR(50) NOT NULL,
-  `postalCode` VARCHAR(15) NULL DEFAULT NULL,
+  `customerPhone` VARCHAR(50) NOT NULL,
+  `customerAddressLine1` VARCHAR(50) NOT NULL,
+  `customerAddressLine2` VARCHAR(50) NULL DEFAULT NULL,
+  `customerCity` VARCHAR(50) NOT NULL,
+  `customerState` VARCHAR(50) NULL DEFAULT NULL,
+  `customerCountry` VARCHAR(50) NOT NULL,
+  `customerPostalCode` VARCHAR(15) NULL DEFAULT NULL,
   PRIMARY KEY (`customerId`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
@@ -35,18 +35,17 @@ DROP TABLE IF EXISTS `restaurant` ;
 
 CREATE TABLE IF NOT EXISTS `restaurant` (
   `restaurantId` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `phone` VARCHAR(50) NOT NULL,
-  `addressLine1` VARCHAR(50) NULL,
-  `addressLine2` VARCHAR(50) NULL DEFAULT NULL,
-  `city` VARCHAR(50) NOT NULL,
-  `state` VARCHAR(50) NULL DEFAULT NULL,
-  `country` VARCHAR(50) NOT NULL,
-  `postalCode` VARCHAR(15) NOT NULL,
+  `restaurantName` VARCHAR(45) NOT NULL,
+  `restaurantPhone` VARCHAR(50) NOT NULL,
+  `restaurantAddressLine1` VARCHAR(50) NULL,
+  `restaurantAddressLine2` VARCHAR(50) NULL DEFAULT NULL,
+  `restaurantCity` VARCHAR(50) NOT NULL,
+  `restaurantState` VARCHAR(50) NULL DEFAULT NULL,
+  `restaurantCountry` VARCHAR(50) NOT NULL,
+  `restaurantPostalCode` VARCHAR(15) NOT NULL,
   PRIMARY KEY (`restaurantId`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
-
 
 
 -- -----------------------------------------------------
@@ -68,18 +67,18 @@ DROP TABLE IF EXISTS `employee` ;
 
 CREATE TABLE IF NOT EXISTS `employee` (
   `employeeId` INT(11) NOT NULL AUTO_INCREMENT,
-  `lastName` VARCHAR(50) NOT NULL,
-  `firstName` VARCHAR(50) NOT NULL,
-  `email` VARCHAR(100) NOT NULL,
+  `employeeLastName` VARCHAR(50) NOT NULL,
+  `employeeFirstName` VARCHAR(50) NOT NULL,
+  `employeeEmail` VARCHAR(100) NOT NULL,
   `restaurantId` INT NOT NULL,
-  `reportsTo` INT(11) NULL DEFAULT NULL,
-  `jobTitle` VARCHAR(50) NOT NULL,
-  `userName` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NULL,
+  `employeeReportsTo` INT(11) NULL DEFAULT NULL,
+  `employeeJobTitle` VARCHAR(50) NOT NULL,
+  `employeeUserName` VARCHAR(45) NOT NULL,
+  `employeePassword` VARCHAR(45) NULL,
   `roleId` INT NOT NULL,
   PRIMARY KEY (`employeeId`),
   CONSTRAINT `employees_ibfk_1`
-    FOREIGN KEY (`reportsTo`)
+    FOREIGN KEY (`employeeReportsTo`)
     REFERENCES `employee` (`employeeId`),
   CONSTRAINT `restaurantId`
     FOREIGN KEY (`restaurantId`)
@@ -94,12 +93,11 @@ CREATE TABLE IF NOT EXISTS `employee` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
-CREATE INDEX `reportsTo` ON `employee` (`reportsTo` ASC);
+CREATE INDEX `reportsTo` ON `employee` (`employeeReportsTo` ASC);
 
 CREATE INDEX `restaurantId_idx` ON `employee` (`restaurantId` ASC);
 
 CREATE INDEX `roleId_idx` ON `employee` (`roleId` ASC);
-
 
 
 -- -----------------------------------------------------
@@ -111,14 +109,14 @@ CREATE TABLE IF NOT EXISTS `order` (
   `orderId` INT(11) NOT NULL AUTO_INCREMENT,
   `orderDate` DATE NOT NULL,
   `customerId` INT(11) NULL,
-  `tableNumber` INT NOT NULL,
+  `orderTableNumber` INT NOT NULL,
   `employeeId` INT NULL,
   `restaurantId` INT NULL,
   PRIMARY KEY (`orderId`),
-  CONSTRAINT `orders_ibfk_11`
+  CONSTRAINT `FK_orders_ibfk_1`
     FOREIGN KEY (`customerId`)
     REFERENCES `customer` (`customerId`),
-  CONSTRAINT `restaurantId_1`
+  CONSTRAINT `FK_order_restaurantId`
     FOREIGN KEY (`restaurantId`)
     REFERENCES `restaurant` (`restaurantId`)
     ON DELETE NO ACTION
@@ -138,11 +136,11 @@ DROP TABLE IF EXISTS `product` ;
 
 CREATE TABLE IF NOT EXISTS `product` (
   `productId` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(70) NOT NULL,
-  `vendor` VARCHAR(50) NOT NULL,
-  `description` TEXT NOT NULL,
-  `buyPrice` DOUBLE NOT NULL,
-  `price` DOUBLE NOT NULL,
+  `productName` VARCHAR(70) NOT NULL,
+  `productVendor` VARCHAR(50) NOT NULL,
+  `productDescription` TEXT NOT NULL,
+  `productBuyPrice` DOUBLE NOT NULL,
+  `productPrice` DOUBLE NOT NULL,
   PRIMARY KEY (`productId`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
@@ -157,14 +155,14 @@ CREATE TABLE IF NOT EXISTS `orderdetail` (
   `orderdetailId` INT NOT NULL AUTO_INCREMENT,
   `orderId` INT(11) NOT NULL,
   `productId` INT NOT NULL,
-  `quantityOrdered` INT(11) NOT NULL,
-  `priceEach` DOUBLE NOT NULL,
-  `chair` VARCHAR(45) NULL,
+  `orderdetailQuantity` INT(11) NOT NULL,
+  `orderdetailPrice` DOUBLE NOT NULL,
+  `orderdetailChair` VARCHAR(45) NULL,
   PRIMARY KEY (`orderdetailId`),
-  CONSTRAINT `orderdetails_ibfk_1`
+  CONSTRAINT `FK_orderdetail_orderdetails_ibfk`
     FOREIGN KEY (`orderId`)
     REFERENCES `order` (`orderId`),
-  CONSTRAINT `orderdetails_ibfk_2`
+  CONSTRAINT `FK_orderdetail_orderdetails_ibfk_2`
     FOREIGN KEY (`productId`)
     REFERENCES `product` (`productId`))
 ENGINE = InnoDB
@@ -182,14 +180,14 @@ CREATE TABLE IF NOT EXISTS `stock` (
   `stockId` INT NOT NULL AUTO_INCREMENT,
   `productId` INT NOT NULL,
   `restaurantId` INT NOT NULL,
-  `quantityInStock` DECIMAL(4,2) NOT NULL DEFAULT 0,
+  `stockQuantity` DECIMAL(4,2) NOT NULL DEFAULT 0,
   PRIMARY KEY (`stockId`, `restaurantId`, `productId`),
-  CONSTRAINT `FK_productId_1`
+  CONSTRAINT `FK_stock_productId`
     FOREIGN KEY (`productId`)
     REFERENCES `product` (`productId`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `FK_restaurantId_1`
+  CONSTRAINT `FK_stock_restaurantId`
     FOREIGN KEY (`restaurantId`)
     REFERENCES `restaurant` (`restaurantId`)
     ON DELETE NO ACTION
