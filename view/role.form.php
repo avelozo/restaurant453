11c2e61edacafffe1b44bd757891ccdb184cdd15
ischneider <ischneider@localhost>
2014-11-29 16:41:33 -0600

    Role adapted to layout.

--- view/role.form.php
+++ view/role.form.php
@@ -1,14 +1,14 @@
 <?php  include 'header.php';?>
 <div class="boxContainer marginContainer">
 	<span>Role</span><hr/>
-		<form  action="<?php /*echo $action; */?>" method="post">
-			<div><label for="id">Id<input type="text" class="inputContent" name="id"
-				id="id" value="<?php /*echo $role->id; */?>"></label></div>
+		<form  action="?<?php echo $action; ?>" method="post">
+			<div><label for="id">ID <input type="text" class="inputContent" name="id"
+				id="id" value="<?php echo $role->id; ?>"></label></div>
 			<div><label for="name">Name <input type="text" class="inputContent" name="name"
-				id="name" value="<?php /*echo $role->name; */?>"></label></div>
+				id="name" value="<?php echo $role->name; ?>"></label></div>
 			<div>
-				<input type="hidden" name="id" value="<?php /*echo $id;*/ ?>">
-				<input type="submit" class="submitButton  rightPosition" value="Confirm<?php /*echo $button; */?>">
+				<input type="hidden" name="id" value="<?php echo $role->id; ?>">
+				<input type="submit" class="submitButton  rightPosition" value="Confirm">
 			</div>
 		</form>
 	</div>
