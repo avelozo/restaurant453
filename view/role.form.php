9f81154bebad881df8d6ee665c7613913e88a928
ischneider <ischneider@localhost>
2014-12-01 13:28:16 -0600

    Restaurant adapted to layout.

--- view/role.form.php
+++ view/role.form.php
@@ -1,16 +1,16 @@
 <?php  include 'header.php';?>
 <div class="boxContainer marginContainer">
 	<span>Role</span><hr/>
-		<form  action="?<?php echo $action; ?>" method="post">
-			<div><label for="id">ID <input type="text" class="inputContent" name="id"
-				id="id" value="<?php echo $role->id; ?>"></label></div>
-			<div><label for="name">Name <input type="text" class="inputContent" name="name"
-				id="name" value="<?php echo $role->name; ?>"></label></div>
-			<div>
-				<input type="hidden" name="id" value="<?php echo $role->id; ?>">
-				<input type="submit" class="submitButton  rightPosition" value="Confirm">
-			</div>
-		</form>
-	</div>
-	</body>
+	<form  action="?<?php echo $action; ?>" method="post">
+		<div><label for="id">ID <input type="text" class="inputContent" name="id"
+			id="id" value="<?php echo $role->id; ?>"></label></div>
+		<div><label for="name">Name <input type="text" class="inputContent" name="name"
+			id="name" value="<?php echo $role->name; ?>"></label></div>
+		<div>
+			<input type="hidden" name="id" value="<?php echo $role->id; ?>">
+			<input type="submit" class="submitButton  rightPosition" value="Confirm">
+		</div>
+	</form>
+</div>
+</body>
 </html>
