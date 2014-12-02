b09638871e99b784eb4c4579f747583312b87e8f
alevelozo <alessandrac.velozo@gmail.com>
2014-12-02 12:16:37 -0600

    Id disabled, buttons added to the leftpanel

--- view/role.form.php
+++ view/role.form.php
@@ -2,7 +2,7 @@
 <div class="boxContainer marginContainer">
 	<span>Role</span><hr/>
 	<form  action="?<?php echo $action; ?>" method="post">
-		<div><label for="id">ID <input type="text" class="inputContent" name="id"
+		<div><label for="id">ID <input type="text" disabled class="inputContent" name="id"
 			id="id" value="<?php echo $role->id; ?>"></label></div>
 		<div><label for="name">Name <input type="text" class="inputContent" name="name"
 			id="name" value="<?php echo $role->name; ?>"></label></div>
