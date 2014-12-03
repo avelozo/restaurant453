f5e7193f5c34ff53f2627673b52947714540b5e4
alevelozo <avelozo@users.noreply.github.com>
2014-12-02 18:58:31 -0600

    Employee, Restaurant validation

---
+++ dataaccess/config.inc.php
@@ -0,0 +1,5 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'roussef';
+    $username = 'root';
+    $password = '123456';
---
+++ mapper/roleMapper.php
@@ -0,0 +1,58 @@
+<?php
+
+	include_once DIR_BASE . "dataaccess/roleDA.php";
+	include_once DIR_BASE . "model/model.role.php";
+
+	class RoleMapper
+	{
+		private $roleDAO = null;
+
+		function RoleMapper()
+		{
+			$this->roleDAO = new RoleDA();
+		}
+
+		public function getRoles($id = null)
+		{
+			$rolesRet = [];
+
+			$roles = $this->roleDAO->getRoles($id);
+
+			foreach ($roles as $role)
+			{
+				array_push($rolesRet, $this->createRole($role));
+			}
+
+			return $rolesRet;
+		}
+		
+		public function addRole($role)
+		{
+			return $this->roleDAO->addRole($role);
+		}
+
+		public function updateRole($role)
+		{
+			return $this->roleDAO->updateRole($role);
+		}
+
+		public function deleteRole($id)
+		{
+			$employees = $this->roleDAO->countEmployees($id);
+			
+			if ($employees > 0)
+				return "Not possible to delete. The role is associated to at least 1 employee.";
+		
+			return $this->roleDAO->deleteRole($id);
+		}
+
+		public function createRole($role)
+		{
+			$rol = new Role();
+
+			$rol->id = $role['roleId'];
+			$rol->name = $role['roleName'];
+
+			return $rol;
+		}
+	}
