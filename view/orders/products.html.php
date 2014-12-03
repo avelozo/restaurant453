84393d9e1e80d4524decd3f2cf1321c1fcfa4756
alevelozo <alessandrac.velozo@gmail.com>
2014-12-03 02:10:16 -0600

    Updating order Products

--- view/orders/products.html.php
+++ view/orders/products.html.php
@@ -1,6 +1,19 @@
 <div>
-	<?php include "orderProducts.php"; 
-	$productList= new ProductList();
+	<?php 
+	include "../../config.php";
+	 include DIR_BASE . 'business/orderBS.php';
+	include "orderProducts.php"; 
+	
+
+	$order= new OrderBS();
+	$order= $order->getOrders(1);
+	$productList= new ProductList($order);
+?>
+	<form  action="?<?php echo $action; ?>" method="post" name="formEmployee" >
 	echo $productList->showProducts(); 
+
+	<input type="text" name="quantity" value="<?php $productQuantity ?>"> 
+	<input type="submit" class="submitButton" value="Confirm">
+	</form>
 	?>
 </div>
